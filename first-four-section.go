1f987106edfcb5ef0f00e0b743d8a6a69799471c
bright adams <btadamz@gmail.com>
2023-12-25 17:36:36 +0100

    last commit for the day

--- first-four-section.go
+++ first-four-section.go
@@ -2,43 +2,100 @@ package main
 
 import (
 	"fmt"
-	"math"
+	// "math"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
 
-	// Values Session 2 ----------------------
-	fmt.Println("go" + "lang")
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
-	fmt.Println(false && true)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	// // Values Session 2 ----------------------
+	// fmt.Println("go" + "lang")
+	// fmt.Println("1+1 =", 1+1)
+	// fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// fmt.Println(false && true)
+	// fmt.Println(true || false)
+	// fmt.Println(!true)
 
-	// Variable Session 3 ----------------------
-    var a = "initial"
-    fmt.Println(a)
-    var b, c int = 1, 2
-    fmt.Println(b, c)
-	var d = true
-    fmt.Println(d)
-	var e int
-    fmt.Println(e)
-	f := "apple"
-    fmt.Println(f)
+	// // Variable Session 3 ----------------------
+    // var a = "initial"
+    // fmt.Println(a)
+    // var b, c int = 1, 2
+    // fmt.Println(b, c)
+	// var d = true
+    // fmt.Println(d)
+	// var e int
+    // fmt.Println(e)
+	// f := "apple"
+    // fmt.Println(f)
 
-	// Constants ----------------------
-	const s string = "constant"
-	const n = 500000000
-	const t = 3e20 / n
-    fmt.Println(d)
-	fmt.Println(int64(t))
-	fmt.Println(math.Sin(n))
+	// // Constants ----------------------
+	// const s string = "constant"
+	// const n = 500000000
+	// const t = 3e20 / n
+    // fmt.Println(d)
+	// fmt.Println(int64(t))
+	// fmt.Println(math.Sin(n))
 
 
 	// for 
+
+
+
+
+
+
+    // i := 1
+    // for i <= 3 {
+    //     fmt.Println(i)
+    //     i = i + 1
+    // }
+
+    // for j := 7; j <= 9; j++ {
+    //     fmt.Println(j)
+    // }
+
+    // for {
+    //     fmt.Println("loop")
+    //     break
+    // }
+
+    // for n := 0; n <= 5; n++ {
+    //     if n%2 == 0 {
+    //         continue
+    //     }
+    //     fmt.Println(n)
+    // }
+ 
+	// if else conditions
+
 	
+ 
+	
+		if 7%2 == 0 {
+			fmt.Println("7 is even")
+		} else {
+			fmt.Println("7 is odd")
+		}
+	
+		if 8%4 == 0 {
+			fmt.Println("8 is divisible by 4")
+		}
+	
+		if 7%2 == 0 || 8%2 == 0 {
+			fmt.Println("either 8 or 7 are even")
+		}
+	
+		if num := 9; num < 0 {
+			fmt.Println(num, "is negative")
+		} else if num < 10 {
+			fmt.Println(num, "has 1 digit")
+		} else {
+			fmt.Println(num, "has multiple digits")
+		}
+	
+
+
+
 }
 
 // Learning-Golang
